30559c5ded7a2469ae9ff5b222f306e24194c3b2
Vignesh150493 <vignesh150493@gmail.com>
2018-11-01 10:08:13 +0530

    WillPopScope widget for back button handling

--- lib/pages/product_detail.dart
+++ lib/pages/product_detail.dart
@@ -8,25 +8,32 @@ class ProductDetail extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: Text(title),
-        ),
-        body: Column(
-          //Cross axis is horizontal centering
-          //Main axis is vertical
-          children: <Widget>[
-            Image.asset(imageUrl),
-            Container(padding: EdgeInsets.all(10.0), child: Text(title)),
-            Container(
-              padding: EdgeInsets.all(10.0),
-              child: RaisedButton(
-                color: Theme.of(context).accentColor,
-                child: Text('Delete'),
-                onPressed: () => Navigator.pop(context, true),
-              ),
-            )
-          ],
-        ));
+    return WillPopScope(
+      //Onwillpop we are blocking the default back pressed behaviour here.
+      onWillPop: () {
+        Navigator.pop(context, false);
+        return Future.value(false);
+      },
+      child: Scaffold(
+          appBar: AppBar(
+            title: Text(title),
+          ),
+          body: Column(
+            //Cross axis is horizontal centering
+            //Main axis is vertical
+            children: <Widget>[
+              Image.asset(imageUrl),
+              Container(padding: EdgeInsets.all(10.0), child: Text(title)),
+              Container(
+                padding: EdgeInsets.all(10.0),
+                child: RaisedButton(
+                  color: Theme.of(context).accentColor,
+                  child: Text('Delete'),
+                  onPressed: () => Navigator.pop(context, true),
+                ),
+              )
+            ],
+          )),
+    );
   }
 }
