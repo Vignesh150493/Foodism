993f603035f7b141e6d521e56d6391074b25ceb2
Vignesh150493 <vignesh150493@gmail.com>
2018-11-02 10:08:14 +0530

    Adding Drawer and Tabs. Temporary implementation for drawer navigation though.

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,10 +1,27 @@
 import 'package:flutter/material.dart';
 import '../product_manager.dart';
+import 'product_admin.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        drawer: Drawer(
+          child: Column(
+            children: <Widget>[
+              AppBar(
+                automaticallyImplyLeading: false,
+                title: Text("Choose"),
+              ),
+              ListTile(
+                title: Text("Manage Products"),
+                onTap: () {
+                  Navigator.push(context, MaterialPageRoute(builder: (context) => ProductAdmin()));
+                },
+              )
+            ],
+          ),
+        ),
         appBar: AppBar(
           title: Text('EasyList'),
         ),
